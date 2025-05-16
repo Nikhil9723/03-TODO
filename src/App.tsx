05d9145f5e1a9c7e977760090b113890ba9f5f55
Nikhil Prajapati <nikhil.prajapati@simformsolutions.com>
2025-05-16 14:39:15 +0530

    FEATURE: remove app.css file from app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { Outlet, Route, Routes } from "react-router-dom";
-import "./App.css";
 import Layout from "./components/layout/Layout";
 import Home from "./pages/Home";
 import About from "./pages/About";
