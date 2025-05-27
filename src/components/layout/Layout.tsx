f8b66f81a6b284c550fbe13253c7bcec8e5d80b3
Nikhil Prajapati <nikhil.prajapati@simformsolutions.com>
2025-05-27 15:46:27 +0530

    Modify: Layou.tsx fix the import order

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,11 +1,12 @@
-import Navbar from "../Navbar";
-import { Outlet } from "react-router-dom";
+import { Outlet } from 'react-router-dom';
+
+import Navbar from '../Navbar';
 
 export default function Layout() {
   return (
     <>
       <Navbar />
-      <main className="w-full flex flex-col items-center gap-2">
+      <main className='w-full flex flex-col items-center gap-2'>
         <Outlet />
       </main>
     </>
