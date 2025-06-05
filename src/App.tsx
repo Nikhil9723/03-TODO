6744dfc2cbf688eee288690a0644c6ffba214a67
Nikhil Prajapati <nikhil.prajapati@simformsolutions.com>
2025-06-05 17:31:54 +0530

    Update: import order in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import { Outlet, Route, Routes } from "react-router-dom";
+import { Outlet, Route, Routes } from 'react-router-dom';
 
-import Layout from "./components/layout/Layout";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Todo from "./pages/Todo";
-import EditTodo from "./pages/EditTodo";
-import { TodoProvider } from "./store/ContextProvider";
-import NotFound from "./pages/NotFound";
+import Layout from './components/layout/Layout';
 
+import Home from './pages/Home';
+import About from './pages/About';
+import Todo from './pages/Todo';
+import EditTodo from './pages/EditTodo';
+
+import { TodoProvider } from './store/ContextProvider';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
